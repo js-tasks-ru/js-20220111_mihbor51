e2a742d8083bc8effce4c67d4ccaed93dcff0be0
Mikhail Borisov <mihbor51@icloud.com>
2022-01-25 19:48:08 +0300

    added: 3-d task

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,28 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-    
+  if (size === undefined) {
+    return string;
+  }
+
+  if (size === 0) {
+    return '';
+  }
+  
+  let counter = 0;
+  const result = [];
+
+  for (const symbol of string) {
+    if (result[result.length - 1] === symbol) {
+      if (counter < size) {
+        result.push(symbol);
+        counter += 1;
+      }
+    } else {
+      counter = 1;
+      result.push(symbol);
+    }
+  }
+
+  return result.join('');
 }
